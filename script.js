6edca0e1387fb69b72390b3879b04a83d63226a6
laszlo.t.flora@gmail.com <laszlo.t.flora@gmail.com>
2021-12-03 15:07:01 +0100

    async await

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function loadEvent() {
-    // console.log('The page has loaded');
+const renderCountryNames = (countries) => {
+    let returnHTML = "";
+    for (const c of countries) {
+        returnHTML += `
+        <div>${c.name.official}</div>
+        `
+    }
+    return returnHTML;
+}
+
+const renderCountryFlags = (countries) => {
+    let returnHTML = "";
+    for (const c of countries) {
+        returnHTML += `
+        <img src="${c.flags.png}">
+        `
+    }
+    return returnHTML;
+}
+
+
+async function loadEvent() {
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+/*     
+    document.getElementById("root").insertAdjacentHTML("beforeend", renderCountryNames(countryArr));
+ */    
+    document.getElementById("root").insertAdjacentHTML("beforeend", `<div id="country-names">${renderCountryNames(countryArr)})</div>`);
+    document.getElementById("root").insertAdjacentHTML("beforeend", `<div id="country-flags">${renderCountryFlags(countryArr)})</div>`);
 }
 
 window.addEventListener("load", loadEvent);
